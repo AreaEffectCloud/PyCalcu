9faad93e0f4875c0388e58b5880d10cee86b85be
AreaEffectCloud <areaeffectcloud.256@gmail.com>
2023-01-13 11:43:55 +0900

    cant execute transfer_latex

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ sg.theme("Reddit")
 ##### ##### Layout  #####  #####
 
 #must resize
-image_formula_latex_column = sg.Image(filename="result.png", key="image_formula_latex", right_click_menu=click_menu[lang])
+image_formula_latex_column = sg.Image(filename="result.png", key="image_formula_latex")
 
 #####   Tab Group #####
 main_column_left_tabs = sg.TabGroup([[
     sg.Tab(normal[lang], normal_layout, key="normal"),
     sg.Tab(function[lang], function_layout, key="function"),
-    sg.Tab(alphabet[lang], alphabet_layout, key="alphabet")
+    sg.Tab(alphabet[lang], alphabet_layout, key="alphabet"),
     ]], font=FONT, expand_x=True)
 
 main_column_right_tabs = sg.TabGroup([[
@@ -23,17 +23,16 @@ main_column_right_tabs = sg.TabGroup([[
     sg.Tab(sigma[lang], sigma_layout, key="sigma", font=FONT),
     sg.Tab(diff[lang], differential_layout, key="diff", font=FONT),
     sg.Tab(integral[lang], integral_layout, key="integral", font=FONT),
-    #No There # sg.Tab(matrix[lang], matrix_layout, key="matrix", font=FONT)
     ]], font=FONT, expand_x=True)
 
 #####   Output  #####
-multiline_formula_tex = sg.Output(key='output_tex', font=FONT_output, pad=((0, 0), (0, 0)), size=(100, 5), expand_x=True, expand_y=True)
+multiline_formula_tex = sg.Output(key='output_tex', font=FONT_output, pad=((0, 0), (0, 0)), size=(100, 5), expand_x=True)
 output_frame = sg.Frame(output_frame_title[lang], [[multiline_formula_tex]], font=FONT_output, expand_x=True)
 
 #レイアウト
 layout = [ [output_frame], 
             [main_column_left_tabs, main_column_right_tabs],
-            [sg.Column([[image_formula_latex_column]],  justification='center', scrollable=True)]
+            #[sg.Column([[image_formula_latex_column]],  justification='center', scrollable=True)]
             ]
 
 window = sg.Window(title[lang], layout, icon="", use_default_focus=False, resizable=True, finalize=True)
@@ -80,16 +79,8 @@ while True:
     elif event in brackets:
         if focus != "":
             text = values["{i}".format(i=focus)]
-            text = text + window[event].get_text() + "(" #type:ignore
+            text = text + window[event].get_text() + "{" #type:ignore
             window["{i}".format(i=focus)].update(text)
-    
-    #2乗
-    elif event in "power_twice":
-        if focus != "":
-            text = values["{i}".format(i=focus)]
-            if text != "":
-                text = text + "²"
-                window["{i}".format(i=focus)].update(text)
 
     #Other Bottons
     elif event in all_btns_keys:
@@ -99,15 +90,37 @@ while True:
             text = text + window[event].get_text() #type:ignore
             window["{i}".format(i=focus)].update(text)
     
+    #2乗
+    elif event in "power_twice":
+        if focus != "":
+            text = values["{i}".format(i=focus)]
+            if text != "":
+                text = text + "²"
+                window["{i}".format(i=focus)].update(text)
+    
     #Add
     elif event in "add_limit":
         if values["limit_formula"] != "":
             if values["limit_start"] != "" and values["limit_end"] != "":
-                print(f'G')
+                window["output_tex"].update(space) #type:ignore
+                #tex = "\\lim_{{0} \to {1}}{2}".format(values["limit_start"], values["limit_end"], values["limit_formula"])
+
+                print(values["limit_start"], values["limit_end"], values["limit_formula"])
+
+            if values["limit_start"] == "" and values["limit_end"] == "":
+                window["output_tex"].update(space) #type:ignore
+                print(transfer_latex(values["limit_formula"]))
 
     elif event in "add_sum":
         if values["sum_formula"] != "":
-            print(f'G')
+            if values["sum_func"] != "":
+                #関数のみの数列
+                if values["sum_end"] == "" and values["sum_start"] == "":
+                    print()
+                #全て入力された状態
+                elif values["sum_end"] != "" and values["sum_start"] != "":
+                    print()
+
 
     elif event in "add_diff":
         if values["diff_formula"] != "":
@@ -149,15 +162,5 @@ while True:
 
         window["output_tex"].print(tex_result) #type:ignore
         print(f"途中式は、 ", values["output_tex"], " です。")
-
-    elif event == 'limit_btn':
-        print(f"pressed it")
-        y = sympy.sin(x ** 2) / (E ** (-x) + 1)
-        tex_result = sympy.latex(sympy.integrate(y, (x, -(Pi / 2), Pi / 2)).doit())
-        latex_result = r"""$${tex}$$""".format(tex=tex_result)
-        image_result = sympy.preview(latex_result, viewer="file", filename="result.png", euler=False, dvioptions=["-T", "tight", "-z", "0", "--truecolor", "-D 600"])
-        print(latex_result)
-        #tex
-        window["output_tex"].print(tex_result) #type:ignore
 
 window.close()
